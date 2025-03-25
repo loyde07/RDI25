fea0a06b685266bf4bb0d04ab0fb89b7d73c3b62
raimanta215 <r.macedon@students.ephec.be>
2025-03-25 16:30:02 +0100

    copie de la base de main pour travailler sur dev
    voir modele workflow

---
+++ api_backend/server.js
@@ -0,0 +1,29 @@
+import express from "express" //version js const express = require('express');
+import dotenv from "dotenv";
+import { connectDB } from "../config/db.js";
+
+import routesLocal from "../routes/routes.local.js";
+
+dotenv.config();
+// get pour récupérer des infos
+//post pour créer des nouvelles data
+//put ou patch pour mettre à jour 
+//delete pour supprimer
+const app = express();
+
+app.use(express.json()); //permet d'accepeter du JSOn dans le req.body
+
+app.use("/api/locals", routesLocal); //origine des routes pour les locaux
+
+app.get("/", (req, res) => {
+    res.send("Server is ready");
+
+});
+
+console.log(process.env.MONGO_URI);
+
+app.listen(process.env.PORT, () => {
+    connectDB();
+    console.log("serveur lancé sur http://localhost:5000 ");
+
+});
---
+++ config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+
+export const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI); // se connecte à la DB en asynchrone
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error){
+        console.error(`Error: ${error.message}`);
+        process.exit(1); // 1 c'est pour dire que y a eu erreur et 0 succès 
+    }
+}
+
+//NoSQL enrgistre dans des collections avec des documents 
