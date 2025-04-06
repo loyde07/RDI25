8c57e96c503b25dad46dd890cb062f41f1b5738f
Marry3005 <m.pierrard@students.ephec.be>
2025-04-06 14:47:14 +0200

    ajout de bouton

--- web_frontend/src/pages/header.jsx
+++ web_frontend/src/pages/header.jsx
@@ -4,6 +4,10 @@ function Header(){
     return(
         <header>
             <h1>WELCOME TO VALORANT</h1>
+            <button onClick={() => navigate('')}>Team</button>
+            <button onClick={() => navigate('')}>Score</button>
+            <button onClick={() => navigate('')}>Tournement</button>
+            <button onClick={() => navigate('')}>Register</button>
         </header>
     );
 
