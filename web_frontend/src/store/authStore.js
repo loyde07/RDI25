f25a66ab9eb5779f4e20597d1bde28f7cc0d9e50
Hassan-mumu <alh.diallo@students.ephec.be>
2025-05-29 16:02:04 +0200

    fix des  appel API

--- web_frontend/src/store/authStore.js
+++ web_frontend/src/store/authStore.js
@@ -2,8 +2,7 @@ import { create } from "zustand";
 import axios from "axios";
 import toast from "react-hot-toast";
 
-//const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
-const API_URL = import.meta.env.VITE_API + "/api/auth";
+const API_URL = import.meta.env.MODE === "production" ? "/api/auth" :"http://localhost:5000/api/auth" ;
 axios.defaults.withCredentials = true;
 
 export const useAuthStore = create((set) => ({
